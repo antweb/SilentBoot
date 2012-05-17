c3aee3c998c822418bff13562978a39f5e5600a1
Anton Weber <mail@antweb.me>
2012-05-17 01:42:37 +0100

    Version 1.1

--- src/com/antweb/silentboot/BootReceiver.java
+++ src/com/antweb/silentboot/BootReceiver.java
@@ -5,26 +5,63 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
-import android.util.Log;
+import android.provider.Settings;
+//import android.util.Log;
 
+/**
+ * Restores previous state at boot-up.
+ * 
+ * @author Anton Weber (antweb)
+ * @version 1.1
+ */
 public class BootReceiver extends BroadcastReceiver {
 	
 	public static final String PREFS_NAME = "silentbootpref";
 	
+	/**
+	 * onReceive method
+	 * 
+	 * @param context
+	 * @intent intent
+	 */
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.d("SilentBoot","BootReceiver received " + intent.toString());
+		//Log.d("SilentBoot","BootReceiver received " + intent.toString());
 		SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
 		
 		if(settings.getBoolean("enabled", true)) {
 			AudioManager audiomanager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 		    
+			
 			int mode = settings.getInt("lastmode", AudioManager.RINGER_MODE_SILENT);
-			Log.d("SilentBoot","Trying to restore mode " + mode);
+			//Log.d("SilentBoot","Trying to restore mode " + mode);
 			audiomanager.setRingerMode(mode);
-			Log.d("SilentBoot","Mode is " + audiomanager.getRingerMode());
+			//Log.d("SilentBoot","Mode is " + audiomanager.getRingerMode());
+			
+			//Airplane toggle
+			if(settings.getBoolean("airplanetoggle", false)) {
+				
+				//Load previous state
+				int lastairplanemode = settings.getInt("lastairplanemode", -1);
+				
+				//Re-enable previous state if necessary
+				if(lastairplanemode != -1 && lastairplanemode != 0) {
+						
+					//Log.d("SilentBoot","Trying to set airplane mode to "+ lastairplanemode);
+					Settings.System.putInt(context.getContentResolver(),Settings.System.AIRPLANE_MODE_ON, lastairplanemode);
+					
+					Intent changeintent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+					changeintent.putExtra("state", lastairplanemode);
+					context.sendBroadcast(intent);
+
+					//Log.d("SilentBoot","Airplane mode is " + Settings.System.getInt(context.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 0));
+					
+				}
+
+			}
+
 		} else {
-			Log.d("SilentBoot","Silent Boot disabled");
+			//Log.d("SilentBoot","Silent Boot disabled");
 		}
 	}
 }
--- src/com/antweb/silentboot/SilentBoot.java
+++ src/com/antweb/silentboot/SilentBoot.java
@@ -1,7 +1,6 @@
 package com.antweb.silentboot;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.CheckBox;
@@ -9,60 +8,118 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
+/**
+ * Silent Boot activity
+ * 
+ * @author Anton Weber (antweb)
+ * @version 1.1
+ */
+public class SilentBoot extends Activity {
 
-public class SilentBoot extends Activity{
-
+	/**
+	 * Preference file name.
+	 */
 	public static final String PREFS_NAME = "silentbootpref";
 
-	/** Called when the activity is first created. */
+	/**
+	 * onCreate method
+	 * 
+	 * @param savedInstanceState
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.main);
     	SharedPreferences settings	=	getSharedPreferences(PREFS_NAME, 0);
     	CheckBox checkbox_enable	=	(CheckBox) findViewById(R.id.checkBoxEnable);
+    	CheckBox checkbox_airplane	=	(CheckBox) findViewById(R.id.checkBoxAirplaneToggle);
     	TextView textview_status	=	(TextView) findViewById(R.id.textStatus);
+    	TextView textview_airplane	=	(TextView) findViewById(R.id.textAirplaneStatus);
     	
-    	//load status
-    	boolean enabled	=	settings.getBoolean("enabled", false);
+    	//Load status
+    	boolean enabled			=	settings.getBoolean("enabled", false);
+    	boolean airplanetoggle	=	settings.getBoolean("airplanetoggle", false);
     	
-    	//set loaded value on checkbox
+    	//Set loaded value on checkbox
     	checkbox_enable.setChecked(enabled);
+    	checkbox_airplane.setChecked(airplanetoggle);
     	
-    	//set status text
-    	setText(textview_status, enabled);
+    	//Set status text   
+    	setEnabledText(textview_status, enabled);
+    	setAirplaneText(textview_airplane, airplanetoggle);
+  
     	
-    	//checkbox handler
+    	//Checkbox handlers
     	checkbox_enable.setOnCheckedChangeListener(new OnCheckedChangeListener() {
     		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
     		{
     			SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
     			SharedPreferences.Editor editor = settings.edit();
+    			
+    			CheckBox checkbox_airplane	=	(CheckBox) findViewById(R.id.checkBoxAirplaneToggle);
     			TextView textview_status	=	(TextView) findViewById(R.id.textStatus);
+    			TextView textview_airplane	=	(TextView) findViewById(R.id.textAirplaneStatus);
     			
     			if (isChecked) {
     				editor.putBoolean("enabled", true);
-    				setText(textview_status, true);
-    				
-    				//debug
-    				Intent serviceintent	=	new Intent();
-    				serviceintent.setAction("com.antweb.silentboot.SilencerService");
-    				startService(serviceintent);
-    				
+    				setEnabledText(textview_status, true);
+    	        	textview_airplane.setEnabled(true);
+    	        	checkbox_airplane.setEnabled(true);
     	        } else {
     	        	editor.putBoolean("enabled", false);
-    	        	setText(textview_status, false);
+    	        	setEnabledText(textview_status, false);
+    	        	textview_airplane.setEnabled(false);
+    	        	checkbox_airplane.setEnabled(false);
+    	        }
+    	        editor.commit();              
+    		}
+    	});
+    	
+    	checkbox_airplane.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+    		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+    		{
+    			SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+    			SharedPreferences.Editor editor = settings.edit();
+    			TextView textview_airplane	=	(TextView) findViewById(R.id.textAirplaneStatus);
+    			
+    			if (isChecked) {
+    				editor.putBoolean("airplanetoggle", true);
+    				setAirplaneText(textview_airplane, true);
+    				
+    	        } else {
+    	        	editor.putBoolean("airplanetoggle", false);
+    	        	setAirplaneText(textview_airplane, false);
     	        }
     	        editor.commit();              
     		}
     	});
     }
     
-    protected void setText(TextView view, boolean enabled) {
+    /**
+     * Sets the enabled description.
+     * 
+     * @param view
+     * @param enabled
+     */
+    protected void setEnabledText(TextView view, boolean enabled) {
     	if(enabled) {
     		view.setText(R.string.textEnabled);
     	} else {
     		view.setText(R.string.textDisabled);
+    	}
+    }
+    
+    /**
+     * Sets the airplane toggle description.
+     * 
+     * @param view
+     * @param enabled
+     */
+    protected void setAirplaneText(TextView view, boolean enabled) {
+    	if(enabled) {
+    		view.setText(R.string.textAirplaneEnabled);
+    	} else {
+    		view.setText(R.string.textAirplaneDisabled);
     	}
     }
 }
