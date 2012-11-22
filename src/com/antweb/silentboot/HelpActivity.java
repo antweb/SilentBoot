aaf614acff47befcb00780be606b15658a5c4e8a
Anton Weber <ant@antweb.me>
2012-11-22 16:21:52 +0100

    Add skeleton for help activity

---
+++ src/com/antweb/silentboot/HelpActivity.java
@@ -0,0 +1,41 @@
+package com.antweb.silentboot;
+
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class HelpActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_activity);
+
+        if (Build.VERSION.SDK_INT > 11) {
+            enableUp();
+        }
+    }
+
+    @TargetApi(11)
+    protected void enableUp() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
