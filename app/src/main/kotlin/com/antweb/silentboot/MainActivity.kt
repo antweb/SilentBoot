f283f7729645aae47a41868a74cd8d21ab31b560
Anton Weber <anton@anonweb.de>
2021-11-13 18:58:20 +0100

    Rewrite main activity as AppCompatActivity
    The old PreferenceActivity is deprecated and without the extra options,
    the preference layout no longer makes sense.

--- app/src/main/kotlin/com/antweb/silentboot/MainActivity.kt
+++ app/src/main/kotlin/com/antweb/silentboot/MainActivity.kt
@@ -3,90 +3,81 @@ package com.antweb.silentboot
 import android.app.AlertDialog
 import android.app.NotificationManager
 import android.content.Intent
-import android.content.SharedPreferences
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener
-import android.os.Build
 import android.os.Bundle
-import android.preference.CheckBoxPreference
-import android.preference.PreferenceActivity
-import android.preference.PreferenceManager
 import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
+import com.antweb.silentboot.databinding.MainActivityBinding
 
-class MainActivity : PreferenceActivity(), OnSharedPreferenceChangeListener {
-    public override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val PREF_KEY_ENABLED = "enabled"
+    }
+
+    private lateinit var binding: MainActivityBinding
+    private var isEnabled = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        addPreferencesFromResource(R.xml.preferences)
-        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
-        val isEnabled = sharedPrefs.getBoolean("enabled", false)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        isEnabled = sharedPrefs.getBoolean(PREF_KEY_ENABLED, false)
+
+        setLabels()
+        binding.activateToggleButton.setOnClickListener {
+            toggleEnabled()
+        }
+
+        binding.openHelpButton.setOnClickListener {
+            val intent = Intent(this, HelpActivity::class.java)
+            startActivity(intent)
+        }
+
         if (isEnabled) {
             checkPermissions()
         }
-
-        setSummaries()
     }
 
-    override fun onResume() {
-        super.onResume()
-        preferenceScreen.sharedPreferences
-            .registerOnSharedPreferenceChangeListener(this)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        preferenceScreen.sharedPreferences
-            .unregisterOnSharedPreferenceChangeListener(this)
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        if (key == PREF_KEY_ENABLED) {
-            val prefEnabled = findPreference(PREF_KEY_ENABLED) as CheckBoxPreference
-            val enabled = prefEnabled.isChecked
-            if (enabled) {
-                checkPermissions()
-                prefEnabled.setSummary(R.string.summaryEnabled)
-            } else {
-                prefEnabled.setSummary(R.string.summaryDisabled)
-            }
-        }
-    }
-
-    protected fun setSummaries() {
-        val prefEnabled = findPreference(PREF_KEY_ENABLED)
-        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
-
-        if (sharedPrefs.getBoolean(
-                "enabled",
-                false
-            )
-        ) {
-            prefEnabled.setSummary(R.string.summaryEnabled)
+    private fun setLabels() {
+        if (isEnabled) {
+            binding.activateToggleButton.text = getString(R.string.buttonDisable)
+            binding.enabledStatusText.text = getString(R.string.summaryEnabled)
         } else {
-            prefEnabled.setSummary(R.string.summaryDisabled)
+            binding.activateToggleButton.text = getString(R.string.buttonEnable)
+            binding.enabledStatusText.text = getString(R.string.summaryDisabled)
         }
     }
 
-    /**
-     * Check permissions
-     */
-    private fun checkPermissions() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return
+    private fun toggleEnabled() {
+        isEnabled = !isEnabled
+
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val editor = sharedPrefs.edit()
+        editor.putBoolean(PREF_KEY_ENABLED, isEnabled)
+        editor.apply()
+
+        setLabels()
+
+        if (isEnabled) {
+            checkPermissions()
         }
-        val nm = applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (!nm.isNotificationPolicyAccessGranted) {
+    }
+
+    private fun checkPermissions() {
+        val notificationManager = applicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (!notificationManager.isNotificationPolicyAccessGranted) {
             val builder = AlertDialog.Builder(this)
             builder.setMessage(getString(R.string.permissionDialogText))
-            builder.setPositiveButton(getString(R.string.permissionDialogEnable)) { dialogInterface, i ->
+            builder.setPositiveButton(getString(R.string.permissionDialogEnable)) { _, _ ->
                 val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
                 startActivityForResult(intent, 0)
             }
             builder.setNegativeButton(getString(R.string.permissionDialogCancel)) { dialogInterface, i -> dialogInterface.dismiss() }
             builder.show()
         }
-    }
-
-    companion object {
-        const val PREF_KEY_ENABLED = "enabled"
     }
 }
