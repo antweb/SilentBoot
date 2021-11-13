91de1e7f743cd4eacad2be54931be4ea2407a0a5
Anton Weber <anton@anonweb.de>
2021-11-13 19:06:33 +0100

    Remove additional modes from boot receiver

--- app/src/main/kotlin/com/antweb/silentboot/BootReceiver.kt
+++ app/src/main/kotlin/com/antweb/silentboot/BootReceiver.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
 import android.preference.PreferenceManager
-import android.provider.Settings
 
 class BootReceiver : BroadcastReceiver() {
     private val RESTORE_DELAY = 5000
@@ -21,43 +20,7 @@ class BootReceiver : BroadcastReceiver() {
             val audiomanager = context
                 .getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
-            // Classic workaround
             if (mode != -1) audiomanager.ringerMode = mode
-            val lastSysVol = settings.getInt("last_sys_vol", -1)
-            val lastNotificationVol = settings.getInt("last_notification_vol", -1)
-
-            // Extended workaround
-            if (settings.getBoolean(
-                    "extended",
-                    false
-                ) && lastSysVol != -1 && lastNotificationVol != -1
-            ) {
-                audiomanager.setStreamVolume(AudioManager.STREAM_SYSTEM, lastSysVol, 0)
-                audiomanager.setStreamVolume(
-                    AudioManager.STREAM_NOTIFICATION,
-                    lastNotificationVol,
-                    0
-                )
-            }
-
-            // Airplane toggle
-            if (settings.getBoolean("airplanetoggle", false)) {
-                // Load previous state
-                val lastAirplaneMode = settings.getInt("last_airplane_mode", -1)
-
-                // Re-enable previous state if necessary
-                if (lastAirplaneMode != -1 && lastAirplaneMode != 0) {
-                    Settings.System.putInt(
-                        context.contentResolver,
-                        Settings.System.AIRPLANE_MODE_ON, lastAirplaneMode
-                    )
-                    val changeintent = Intent(
-                        Intent.ACTION_AIRPLANE_MODE_CHANGED
-                    )
-                    changeintent.putExtra("state", lastAirplaneMode)
-                    context.sendBroadcast(intent)
-                }
-            }
 
             // Start notification
             if (settings.getBoolean("compatibility", false)) {
