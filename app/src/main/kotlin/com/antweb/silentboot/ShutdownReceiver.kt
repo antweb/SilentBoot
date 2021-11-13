2699ad7f6771493f0dbd88da41af0a154834dec5
Anton Weber <anton@anonweb.de>
2021-11-13 19:07:45 +0100

    Remove additional modes from shutdown receiver

--- app/src/main/kotlin/com/antweb/silentboot/ShutdownReceiver.kt
+++ app/src/main/kotlin/com/antweb/silentboot/ShutdownReceiver.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
 import android.preference.PreferenceManager
-import android.provider.Settings
 
 class ShutdownReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -15,41 +14,9 @@ class ShutdownReceiver : BroadcastReceiver() {
                 .getSystemService(Context.AUDIO_SERVICE) as AudioManager
             val editor = settings.edit()
 
-            // Extended workaround
-            if (settings.getBoolean("extended", false)) {
-                editor.putInt(
-                    "last_sys_vol",
-                    audiomanager.getStreamVolume(AudioManager.STREAM_SYSTEM)
-                )
-                editor.putInt(
-                    "last_notification_vol",
-                    audiomanager.getStreamVolume(AudioManager.STREAM_NOTIFICATION)
-                )
-                audiomanager.setStreamVolume(AudioManager.STREAM_SYSTEM, 0, 0)
-                audiomanager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, 0, 0)
-            }
-
-            // Classic workaround
             editor.putInt("last_ringer_mode", audiomanager.ringerMode)
             audiomanager.ringerMode = AudioManager.RINGER_MODE_SILENT
 
-            // Airplane mode toggle
-            if (settings.getBoolean("airplanetoggle", false)) {
-                // Save current state
-                val lastAirplaneMode = Settings.System.getInt(
-                    context.contentResolver,
-                    Settings.System.AIRPLANE_MODE_ON, 0
-                )
-                editor.putInt("last_airplane_mode", lastAirplaneMode)
-
-                // Deactivate airplane mode if necessary
-                if (lastAirplaneMode != 0) {
-                    Settings.System.putInt(
-                        context.contentResolver,
-                        Settings.System.AIRPLANE_MODE_ON, 0
-                    )
-                }
-            }
             editor.commit()
         }
     }
