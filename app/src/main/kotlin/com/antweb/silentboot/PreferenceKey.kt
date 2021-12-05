a5f9dfc8b24f9b4b079845976055e1759b0a6fce
Anton Weber <anton@anonweb.de>
2021-12-05 22:25:37 +0100

    Enable Do Not Disturb on shutdown

--- app/src/main/kotlin/com/antweb/silentboot/PreferenceKey.kt
+++ app/src/main/kotlin/com/antweb/silentboot/PreferenceKey.kt
@@ -3,4 +3,5 @@ package com.antweb.silentboot
 enum class PreferenceKey(val key: String) {
     ENABLED("enabled"),
     LAST_RINGER_MODE("last_ringer_mode"),
+    LAST_DND_MODE("last_dnd_mode"),
 }
--- app/src/main/kotlin/com/antweb/silentboot/ShutdownReceiver.kt
+++ app/src/main/kotlin/com/antweb/silentboot/ShutdownReceiver.kt
@@ -1,5 +1,6 @@
 package com.antweb.silentboot
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -17,10 +18,19 @@ class ShutdownReceiver : BroadcastReceiver() {
 
         val audioManager = context
             .getSystemService(Context.AUDIO_SERVICE) as AudioManager
-        val editor = sharedPrefs.edit()
+        val notificationManager = context
+            .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        editor.putInt(PreferenceKey.LAST_RINGER_MODE.key, audioManager.ringerMode)
+        val lastRingerMode = audioManager.ringerMode
+        val lastDnd = notificationManager.currentInterruptionFilter
+
+        val editor = sharedPrefs.edit()
+        editor.putInt(PreferenceKey.LAST_RINGER_MODE.key, lastRingerMode)
+        editor.putInt(PreferenceKey.LAST_DND_MODE.key, lastDnd)
+        editor.apply()
+
         audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
+        notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE)
 
         // Stick to commit to be on the safe side?
         editor.commit()
