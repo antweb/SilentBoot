04a96e03d422012bbbfc36f1407dee9719f4b1ae
Anton Weber <anton@anonweb.de>
2021-12-05 18:33:31 +0100

    Change help activity over to AppCompatActivity

--- app/src/main/kotlin/com/antweb/silentboot/HelpActivity.kt
+++ app/src/main/kotlin/com/antweb/silentboot/HelpActivity.kt
@@ -1,25 +1,11 @@
 package com.antweb.silentboot
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
-class HelpActivity : Activity() {
+class HelpActivity : AppCompatActivity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.help_activity)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                val intent = Intent(this, MainActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                startActivity(intent)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
